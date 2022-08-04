a5c6794f870b3c631132774968ceaa4e125dd5d9
Matthew Holt <mholt@users.noreply.github.com>
2022-08-03 19:27:16 -0600

    Automatically trim top level folder prefix

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Caddy embedded file system
 
 This Caddy plugin embeds your site directly into your web server's binary.
 
-**NOTE:** This plugin requires building Caddy from source on your own machine because you need to add your own content before compiling. The `go` command is _required_ and I recommend using [`xcaddy`](https://github.com/caddyserver/xcaddy) to build. You cannot download this plugin from the Caddy website, for example, and expect it to have your site embedded within it.
+**NOTE:** This plugin requires building Caddy from source on your own machine because you need to add your own content to the `files` directory before compiling. The `go` command is _required_ and I recommend using [`xcaddy`](https://github.com/caddyserver/xcaddy) to build. You cannot download this plugin from the Caddy website, for example, and expect it to have your site embedded within it.
 
 ## Instructions
 
@@ -25,13 +25,21 @@ You can customize the `//go:embed` directive in the source before building if yo
 
 ## Site root
 
-Somewhat annoyingly, there doesn't seem to be a way to embed a folder's contents into the root of the file system: you _have_ to prefix filenames with the name of the folder. That means your homepage would be at `https://example.com/files/index.html` by default.
+Somewhat annoyingly, when you use `go:embed` to add a folder, Go embeds the folder to the root of the virtual file system, without a way to configure Go to add its _contents_ to the root. Because our directive is `go:embed files`, that means all filenames have to be prefixed with `files/`. This is unintuitive as you would expect your site root, for example, to be at `index.html`, not `files/index.html`.
 
-To work around this limitation, specify a root of `files`:
+To counter this behavior, this module automatically ["subs the FS"](https://pkg.go.dev/io/fs#Sub) to trim that top-level folder prefix as long as the embedded directory is named `files`, and it is not moved or renamed by you.
+
+I would recommend simply doing as the instructions say, and putting your content into the `files` folder. You can put multiple folders in there if you want more than one. But you are always welcome to do your own thing and change the go:embed directive, etc. If you do that, the automatic prefix stripping won't work for you.
+
+If you are using `file_server` _and if you change the go:embed directive_, you can still strip the root folder name like this:
 
 ```
 file_server {
 	fs embedded
-	root files
+	root myfolder
 }
 ```
+
+where `myfolder` is the name of the folder you added to the `go:embed` directive. **This is only required if you are customizing the go:embed directive.**
+
+In summary: For most people, this module will "just work" and you do not need to set or change the site root.
--- module.go
+++ module.go
@@ -10,17 +10,61 @@ import (
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 )
 
-// The variable below is what will contain your static files.
-// The go command will automatically embed the contents of the
-// files subfolder into this virtual file system. You can
-// optionally change the go:embed directive to embed other files
-// or folders.
-
+// embedded is what will contain your static files. The go command
+// will automatically embed the files subfolder into this virtual
+// file system. You can optionally change the go:embed directive
+// to embed other files or folders.
+//
 //go:embed files
 var embedded embed.FS
 
+// files is the actual, more generic file system to be utilized.
+var files fs.FS = embedded
+
+// topFolder is the name of the top folder of the virtual
+// file system. go:embed does not let us add the contents
+// of a folder to the root of a virtual file system, so
+// if we want to trim that root folder prefix, we need to
+// also specify it in code as a string. Otherwise the
+// user would need to add configuration or code to trim
+// this root prefix from all filenames, e.g. specifying
+// "root files" in their file_server config.
+//
+// It is NOT REQUIRED to change this if changing the
+// go:embed directive; it is just for convenience in
+// the default case.
+const topFolder = "files"
+
 func init() {
 	caddy.RegisterModule(FS{})
+	stripFolderPrefix()
+}
+
+// stripFolderPrefix opens the root of the file system. If it
+// contains only 1 file, being a directory with the same
+// name as the topFolder const, then the file system will
+// be fs.Sub()'ed so the contents of the top folder can be
+// accessed as if they were in the root of the file system.
+// This is a convenience so most users don't have to add
+// additional configuration or prefix their filenames
+// unnecessarily.
+func stripFolderPrefix() error {
+	if f, err := files.Open("."); err == nil {
+		defer f.Close()
+
+		if dir, ok := f.(fs.ReadDirFile); ok {
+			entries, err := dir.ReadDir(2)
+			if err == nil &&
+				len(entries) == 1 &&
+				entries[0].IsDir() &&
+				entries[0].Name() == topFolder {
+				if sub, err := fs.Sub(embedded, topFolder); err == nil {
+					files = sub
+				}
+			}
+		}
+	}
+	return nil
 }
 
 // FS implements a Caddy module and fs.StatFS for an embedded
@@ -29,6 +73,13 @@ func init() {
 // Simply put your files in a subfolder called `files` then
 // build Caddy with your local copy of this plugin. Your
 // site's files will be embedded directly into the binary.
+//
+// If the embedded file system contains only one file in
+// its root which is a folder named "files", this module
+// will strip that folder prefix using fs.Sub(), so that
+// the contents of the folder can be accessed by name as
+// if they were in the actual root of the file system.
+// In other words, before: files/foo.txt, after: foo.txt.
 type FS struct{}
 
 // CaddyModule returns the Caddy module information.
@@ -42,12 +93,12 @@ func (FS) CaddyModule() caddy.ModuleInfo {
 func (FS) Open(name string) (fs.File, error) {
 	// TODO: the file server doesn't clean up leading and trailing slashes, but embed.FS is particular so we remove them here; I wonder if the file server should be tidy in the first place (see also Stat below)
 	name = strings.Trim(name, "/")
-	return embedded.Open(name)
+	return files.Open(name)
 }
 
 func (FS) Stat(name string) (fs.FileInfo, error) {
 	name = strings.Trim(name, "/")
-	file, err := embedded.Open(name)
+	file, err := files.Open(name)
 	if err != nil {
 		return nil, fmt.Errorf("stat: %w", err)
 	}
